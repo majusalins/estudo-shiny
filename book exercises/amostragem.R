73f7182b811d50def422de03da02f5b1920c0462
maju <71796144+majusalins@users.noreply.github.com>
2025-02-21 17:14:19 -0400

    Técnica de reamostragem bootstrap

--- book exercises/amostragem.R
+++ book exercises/amostragem.R
@@ -37,3 +37,34 @@ populacao <- data.frame(grupo = rep(c("A", "B"), times = c(700, 300)),
 
 amostra_estratificada <- populacao %>% group_by(grupo) %>% sample_n(size = 50)
 print(amostra_estratificada)
+
+
+# avaliação de satisfação de um número muito grande de clientes, utilizando a técnica
+# de reamostragem bootstrap
+# passo 1: coletando dados
+set.seed(123)
+satisfacao <- rnorm(1000, mean = 7.5, sd = 1.2)
+
+# passo 2: calcular média e desvio padrão da amostra
+media_amostra <- mean(satisfacao)
+desvio_padrao <- sd(satisfacao)
+cat("Média da amostra:", media_amostra, "\n")
+cat("Desvio padrão da amostra:", desvio_padrao, "\n")
+
+# passo 3: calcular o intervalo de confiança de 95%
+erro_padrao <- desvio_padrao / sqrt(length(satisfacao))
+t_critico <- qt(0.975, df = length(satisfacao) - 1)
+limite_inferior <- media_amostra - t_critico * erro_padrao
+limite_superior <- media_amostra + t_critico * erro_padrao
+cat("IC 95%:[", limite_inferior, ",", limite_superior,"]\n")
+
+# passo 4: aplicar o método bootstrap
+library(boot)
+
+media_boot <- function(data, indices){
+  return(mean(data[indices]))
+}
+
+resultado_boot <- boot(data = satisfacao, statistic = media_boot, R = 1000)
+ic_boot <- boot.ci(resultado_boot, type = "perc")
+print(ic_boot)
